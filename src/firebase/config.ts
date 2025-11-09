34eeffe269ca336e6442e325970cca6014335f45
TABISH <tabishtabish7771@gmail.com>
2025-11-09 13:25:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9324632916-764ff",
+  "appId": "1:829834536535:web:c8d0a34e7684f2c9382512",
+  "apiKey": "AIzaSyApCD5RbSg8ZcMHZelBz_3Zkq3A_wJszpk",
+  "authDomain": "studio-9324632916-764ff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "829834536535"
+};
